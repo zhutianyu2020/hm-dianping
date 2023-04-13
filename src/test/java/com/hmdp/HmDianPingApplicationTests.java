a9523e29f85bc3ff08046cecd6f9de219630927c
userName <1660514602@qq.com>
2023-04-13 17:12:44 +0800

    实战篇-04优惠卷秒杀-实现秒杀下单

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,18 +1,44 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
- @Resource
+    @Resource
     private ShopServiceImpl shopService;
- @Test
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    private ExecutorService es= Executors.newFixedThreadPool(500);
+    @Test
     void testSaveShop() throws InterruptedException {
-     shopService.saveShopToRedis(1L,10L);
- }
+        shopService.saveShopToRedis(1L, 10L);
+    }
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task=()->{
+            for(int i=0;i<100;i++){
+                Long id=redisIdWorker.nextId("order");
+                System.out.println("id="+Long.toBinaryString(id));
+            }
+            latch.countDown();
+        };
+        long begin=System.currentTimeMillis();
+        for (int i=0;i<300;i++){
+            es.submit(task);
+        }
+        latch.await();
+        long end=System.currentTimeMillis();
+        System.out.println("time="+(end-begin));
+    }
+
 
 }
